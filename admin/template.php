4d62e9b58fb3affa61f670cea828206e9dd4ccac
asadhossain <developerasad007@gmail.com>
2023-03-23 02:16:18 +0600

    blog

--- admin/template.php
+++ admin/template.php
@@ -8,19 +8,16 @@
         <?php include_once('includes/sidenav.php') ?>
 
             <div id="layoutSidenav_content">
+               <div class="container-fluid">
+
+
+
+
+
                
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+               </div>
+               <?php include_once('includes/footer.php'); ?>
+
             </div>
         </div>
         <?php include_once('includes/script.php'); ?>
