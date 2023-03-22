a3494c5b5c5aacc58cb14958acc4b2ebb5501eaa
asadhossain <developerasad007@gmail.com>
2023-03-23 03:47:32 +0600

    blog

--- admin/template.php
+++ admin/template.php
@@ -7,15 +7,21 @@
         <div id="layoutSidenav">
         <?php include_once('includes/sidenav.php') ?>
 
-            <div id="layoutSidenav_content">
+            <div id="layoutSidenav_content"> 
+
+                <main>
                <div class="container-fluid">
 
+               <?php 
+                    if(isset($view)){
+                        if($view=="dashboard"){
+                            include('view/dash_view.php');
+                        }
+                    }
+               ?>
 
-
-
-
-               
                </div>
+                </main>
                <?php include_once('includes/footer.php'); ?>
 
             </div>
