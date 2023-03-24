d87ab19d02dd12a4cba761ca3444e1ac18d5a51c
asadhossain <developerasad007@gmail.com>
2023-03-25 02:46:16 +0600

    a

---
+++ admin/template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once('includes/head.php'); ?>
+
+    <body class="sb-nav-fixed">
+       <?php include_once('includes/topnav.php') ?>
+        <div id="layoutSidenav">
+        <?php include_once('includes/sidenav.php') ?>
+
+            <div id="layoutSidenav_content"> 
+
+                <main>
+               <div class="container-fluid">
+
+               <?php 
+                    if(isset($view)){
+                        if($view=="dashboard"){
+                            include('view/dash_view.php');
+                        }
+                    }
+               ?>
+
+               </div>
+                </main>
+               <?php include_once('includes/footer.php'); ?>
+
+            </div>
+        </div>
+        <?php include_once('includes/script.php'); ?>
+
+    </body>
+</html>
